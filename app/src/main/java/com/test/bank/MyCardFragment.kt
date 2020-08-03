7b22085090ac961e5269cb13aa8b106b62fcbdf9
sam <s.bogoslavec@makeapp.com>
2020-08-03 15:14:37 +0300

    Init commit

---
+++ app/src/main/java/com/test/bank/MyCardFragment.kt
@@ -0,0 +1,13 @@
+package com.test.bank
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class MyCardFragment: Fragment(R.layout.fragment_my_cards) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
